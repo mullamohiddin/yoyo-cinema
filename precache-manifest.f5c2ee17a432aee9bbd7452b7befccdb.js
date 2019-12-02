a572c4b55d5ad10b83abf3bb964359e4b1cca604
Venkt <venkatesh.r.putti@gmail.com>
2019-12-02 23:26:45 +0400

    Updates

---
+++ precache-manifest.f5c2ee17a432aee9bbd7452b7befccdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9f4ae2e9460dfb72b10747902fa453",
+    "url": "/yoyo-cinema/index.html"
+  },
+  {
+    "revision": "26557dcb678b06486143",
+    "url": "/yoyo-cinema/static/css/main.53beb417.chunk.css"
+  },
+  {
+    "revision": "dc6dd4156cf17c6c0122",
+    "url": "/yoyo-cinema/static/js/2.0d40c6ca.chunk.js"
+  },
+  {
+    "revision": "26557dcb678b06486143",
+    "url": "/yoyo-cinema/static/js/main.a66534b6.chunk.js"
+  },
+  {
+    "revision": "2647dfa327809f6f9e78",
+    "url": "/yoyo-cinema/static/js/runtime-main.7c2367f9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/yoyo-cinema/static/media/logo.25bf045c.svg"
+  }
+]);
