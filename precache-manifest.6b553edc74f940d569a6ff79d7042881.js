f8c12e0ab81e625aa17840b4c54b66c4c3bb0202
Venkt <venkatesh.r.putti@gmail.com>
2019-12-02 23:08:09 +0400

    Updates

---
+++ precache-manifest.6b553edc74f940d569a6ff79d7042881.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141a5cdb9b38e6c29af1903dd5f9f9d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d34a0febaf066a34cd0a",
+    "url": "/static/css/main.53beb417.chunk.css"
+  },
+  {
+    "revision": "dc6dd4156cf17c6c0122",
+    "url": "/static/js/2.0d40c6ca.chunk.js"
+  },
+  {
+    "revision": "d34a0febaf066a34cd0a",
+    "url": "/static/js/main.f1c863c0.chunk.js"
+  },
+  {
+    "revision": "c11786081a89bd938798",
+    "url": "/static/js/runtime-main.c32c495d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
